c6710ce5da0d6a4151a502f288e451301e307d3b
Dan Brunwasser <me@danb.link>
2021-08-03 21:44:35 -0700

    Updated app model to support lit view diff.

--- app/src/main/java/link/danb/launcher/AppViewModel.kt
+++ app/src/main/java/link/danb/launcher/AppViewModel.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.pm.LauncherApps
 import android.os.UserHandle
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 
 class AppViewModel(application: Application) : AndroidViewModel(application) {
@@ -14,44 +15,29 @@ class AppViewModel(application: Application) : AndroidViewModel(application) {
             .applicationContext
             .getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps
 
-    val apps = MutableLiveData<List<AppItem>>()
+    val apps: LiveData<List<AppItem>> by lazy {
+        val mutableList = ArrayList<AppItem>()
+        val liveData = MutableLiveData<List<AppItem>>(mutableList)
 
-    init {
-        updateApps()
-
-        launcherApps.registerCallback(object : LauncherApps.Callback() {
-            override fun onPackageRemoved(packageName: String?, user: UserHandle?) {
-                updateApps()
+        val addApp = { name: String?, user: UserHandle? ->
+            if (name != null && user != null) {
+                mutableList.addAll(
+                    launcherApps.getActivityList(name, user).map { AppItem(user, it) }
+                )
             }
+        }
 
-            override fun onPackageAdded(packageName: String?, user: UserHandle?) {
-                updateApps()
+        val removeApp = { name: String?, user: UserHandle? ->
+            if (name != null && user != null) {
+                mutableList.removeAll {
+                    it.info.applicationInfo.packageName == name && it.user == user
+                }
             }
+        }
 
-            override fun onPackageChanged(packageName: String?, user: UserHandle?) {
-                updateApps()
-            }
+        val notify = { liveData.value = mutableList }
 
-            override fun onPackagesAvailable(
-                packageNames: Array<out String>?,
-                user: UserHandle?,
-                replacing: Boolean
-            ) {
-                updateApps()
-            }
-
-            override fun onPackagesUnavailable(
-                packageNames: Array<out String>?,
-                user: UserHandle?,
-                replacing: Boolean
-            ) {
-                updateApps()
-            }
-        })
-    }
-
-    private fun updateApps() {
-        apps.value =
+        mutableList.addAll(
             launcherApps
                 .profiles
                 .flatMap { user ->
@@ -59,5 +45,44 @@ class AppViewModel(application: Application) : AndroidViewModel(application) {
                         .getActivityList(null, user)
                         .map { info -> AppItem(user, info) }
                 }
+        )
+
+        launcherApps.registerCallback(object : LauncherApps.Callback() {
+            override fun onPackageRemoved(packageName: String?, user: UserHandle?) {
+                removeApp(packageName, user)
+                notify()
+            }
+
+            override fun onPackageAdded(packageName: String?, user: UserHandle?) {
+                addApp(packageName, user)
+                notify()
+            }
+
+            override fun onPackageChanged(packageName: String?, user: UserHandle?) {
+                removeApp(packageName, user)
+                addApp(packageName, user)
+                notify()
+            }
+
+            override fun onPackagesAvailable(
+                packageNames: Array<out String>?,
+                user: UserHandle?,
+                replacing: Boolean
+            ) {
+                packageNames?.forEach { addApp(it, user) }
+                notify()
+            }
+
+            override fun onPackagesUnavailable(
+                packageNames: Array<out String>?,
+                user: UserHandle?,
+                replacing: Boolean
+            ) {
+                packageNames?.forEach { removeApp(it, user) }
+                notify()
+            }
+        })
+
+        liveData
     }
 }
