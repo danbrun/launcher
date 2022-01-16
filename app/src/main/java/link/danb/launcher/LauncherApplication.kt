97eff88062a8c12c79580ce8d42a3f0abbc806e9
Dan Brunwasser <dan@brunwasser.com>
2022-01-15 21:50:47 -0800

    Updated launcher theme.

---
+++ app/src/main/java/link/danb/launcher/LauncherApplication.kt
@@ -0,0 +1,12 @@
+package link.danb.launcher
+
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+
+class LauncherApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
