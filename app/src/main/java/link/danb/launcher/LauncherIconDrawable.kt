917225630256a39ea3397a3c5498dbcbeb92cfab
Dan Brunwasser <dan@brunwasser.com>
2022-02-02 00:58:15 -0800

    Updated icon rendering with improved padding handling.

--- app/src/main/java/link/danb/launcher/LauncherIconDrawable.kt
+++ app/src/main/java/link/danb/launcher/LauncherIconDrawable.kt
@@ -5,18 +5,14 @@ import android.graphics.*
 import android.graphics.drawable.AdaptiveIconDrawable
 import android.graphics.drawable.Drawable
 import androidx.appcompat.content.res.AppCompatResources
-import kotlin.math.roundToInt
 
 class LauncherIconDrawable(private val context: Context, appItem: AppItem) :
     Drawable() {
 
-    private val size: Float = context.resources.getDimension(R.dimen.launcher_icon_size)
-    private val sizeInt: Int = size.roundToInt()
+    private val size: Int = context.resources.getDimension(R.dimen.launcher_icon_size).toInt()
     private val radius: Float = context.resources.getDimension(R.dimen.launcher_icon_radius)
-    private val foregroundPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private val backgroundPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private val foreground: Bitmap = Bitmap.createBitmap(sizeInt, sizeInt, Bitmap.Config.ARGB_8888)
-    private val background: Bitmap = Bitmap.createBitmap(sizeInt, sizeInt, Bitmap.Config.ARGB_8888)
+    private val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val bitmap: Bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888)
 
     private val defaultBackground: Drawable by lazy {
         AppCompatResources.getDrawable(context, R.drawable.launcher_icon_background)!!
@@ -24,26 +20,41 @@ class LauncherIconDrawable(private val context: Context, appItem: AppItem) :
 
     init {
         val icon = appItem.info.getIcon(0)
+        val canvas = Canvas(bitmap)
+
         if (icon is AdaptiveIconDrawable) {
-            render(icon.foreground, foreground)
-            render(icon.background, background)
+            val padding = (size * AdaptiveIconDrawable.getExtraInsetFraction()).toInt()
+            val bounds = Rect(-padding, -padding, size + padding, size + padding)
+
+            icon.background.run {
+                setBounds(bounds)
+                draw(canvas)
+            }
+            icon.foreground.run {
+                setBounds(bounds)
+                draw(canvas)
+            }
         } else {
-            render(icon, foreground)
-            render(defaultBackground, background)
+            val padding = context.resources.getDimension(R.dimen.launcher_icon_padding).toInt()
+
+            defaultBackground.run {
+                setBounds(0, 0, size, size)
+                draw(canvas)
+            }
+            icon.run {
+                setBounds(padding, padding, size - padding, size - padding)
+                draw(canvas)
+            }
         }
 
-        foregroundPaint.shader =
-            BitmapShader(foreground, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
-        backgroundPaint.shader =
-            BitmapShader(background, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+        paint.shader = BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
     }
 
-    override fun getIntrinsicWidth(): Int = sizeInt
-    override fun getIntrinsicHeight(): Int = sizeInt
+    override fun getIntrinsicWidth(): Int = size
+    override fun getIntrinsicHeight(): Int = size
 
     override fun draw(canvas: Canvas) {
-        canvas.drawRoundRect(0f, 0f, size, size, radius, radius, backgroundPaint)
-        canvas.drawRoundRect(0f, 0f, size, size, radius, radius, foregroundPaint)
+        canvas.drawRoundRect(0f, 0f, size.toFloat(), size.toFloat(), radius, radius, paint)
     }
 
     override fun setAlpha(p0: Int) {
@@ -56,12 +67,5 @@ class LauncherIconDrawable(private val context: Context, appItem: AppItem) :
 
     override fun getOpacity(): Int {
         TODO("Not yet implemented")
-    }
-
-    private fun render(drawable: Drawable, bitmap: Bitmap) {
-        drawable.run {
-            setBounds(0, 0, sizeInt, sizeInt)
-            draw(Canvas(bitmap))
-        }
     }
 }
