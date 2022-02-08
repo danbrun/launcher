f61e2c3a04bf0d7fa0f07ac0feb6bbad7419891b
Dan Brunwasser <dan@brunwasser.com>
2022-02-07 23:32:12 -0800

    Refactored view model with background thread loading.

--- app/src/main/java/link/danb/launcher/AppViewModel.kt
+++ app/src/main/java/link/danb/launcher/AppViewModel.kt
@@ -3,6 +3,7 @@ package link.danb.launcher
 import android.app.ActivityOptions
 import android.app.Application
 import android.content.Context
+import android.content.pm.LauncherActivityInfo
 import android.content.pm.LauncherApps
 import android.graphics.Rect
 import android.os.UserHandle
@@ -11,7 +12,9 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class AppViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -19,38 +22,21 @@ class AppViewModel(application: Application) : AndroidViewModel(application) {
         getApplication<Application>()
             .applicationContext
             .getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps
-
-    private fun getAppItems() = launcherApps.profiles.flatMap { getAppItems(it) }
-
-    private fun getAppItems(user: UserHandle) = getAppItems(null, user)
-
-    private fun getAppItems(packageName: String?, user: UserHandle) =
-        launcherApps.getActivityList(packageName, user).map { AppItem(it) }
-
+    private val activityList: ArrayList<LauncherActivityInfo> = ArrayList()
     private val mutableApps: MutableLiveData<List<AppItem>> = MutableLiveData(ArrayList())
-    val apps: LiveData<List<AppItem>> = mutableApps
-
     private val mutableFilter: MutableLiveData<AppFilter> = MutableLiveData(AppFilter.PERSONAL)
+
+    val apps: LiveData<List<AppItem>> = mutableApps
     val filter: LiveData<AppFilter> = mutableFilter
 
     init {
         viewModelScope.launch {
-            mutableApps.value = getAppItems()
+            withContext(Dispatchers.IO) {
+                updateActivityList()
+                postActivityList()
+            }
             launcherApps.registerCallback(LauncherAppsCallback())
         }
-    }
-
-    fun setFilter(filter: AppFilter) {
-        mutableFilter.value = filter
-    }
-
-    private fun getBounds(view: View): Rect {
-        val pos = IntArray(2).apply { view.getLocationOnScreen(this) }
-        return Rect(pos[0], pos[1], view.width, view.height)
-    }
-
-    private fun getAnimation(view: View): ActivityOptions {
-        return ActivityOptions.makeClipRevealAnimation(view, 0, 0, view.width, view.height)
     }
 
     fun openApp(appItem: AppItem, view: View) =
@@ -69,52 +55,90 @@ class AppViewModel(application: Application) : AndroidViewModel(application) {
             getAnimation(view).toBundle()
         )
 
-    private fun isSamePackage(appItem: AppItem, packageName: String?, user: UserHandle?) =
-        appItem.info.applicationInfo.packageName == packageName && appItem.info.user == user
+    fun setFilter(filter: AppFilter) {
+        mutableFilter.value = filter
+    }
+
+    private fun updateActivityList(user: UserHandle? = null, packageName: String? = null) {
+        if (user != null) {
+            if (packageName != null) {
+                activityList.removeIf {
+                    it.user == user && it.applicationInfo.packageName == packageName
+                }
+            } else {
+                activityList.removeIf {
+                    it.user == user
+                }
+            }
+            activityList.addAll(launcherApps.getActivityList(packageName, user))
+        } else {
+            activityList.clear()
+            activityList.addAll(
+                launcherApps.profiles.flatMap {
+                    launcherApps.getActivityList(null, it)
+                }
+            )
+        }
+    }
+
+    private fun postActivityList() {
+        mutableApps.postValue(activityList.map { AppItem(it) })
+    }
 
     inner class LauncherAppsCallback : LauncherApps.Callback() {
         override fun onPackageRemoved(packageName: String?, user: UserHandle?) {
-            viewModelScope.launch {
-                mutableApps.value =
-                    mutableApps.value!!.filter { !isSamePackage(it, packageName, user) }
-            }
+            updateSinglePackage(packageName, user)
         }
 
         override fun onPackageAdded(packageName: String?, user: UserHandle?) {
-            if (packageName != null && user != null) {
-                viewModelScope.launch {
-                    mutableApps.value =
-                        listOf(mutableApps.value!!, getAppItems(packageName, user)).flatten()
-                }
-            }
+            updateSinglePackage(packageName, user)
         }
 
         override fun onPackageChanged(packageName: String?, user: UserHandle?) {
-            if (packageName != null && user != null) {
-                viewModelScope.launch {
-                    mutableApps.value =
-                        listOf(
-                            mutableApps.value!!.filter { !isSamePackage(it, packageName, user) },
-                            getAppItems(packageName, user)
-                        ).flatten()
-                }
-            }
+            updateSinglePackage(packageName, user)
         }
 
         override fun onPackagesAvailable(
             packageName: Array<out String>?, user: UserHandle?, replacing: Boolean
         ) {
-            viewModelScope.launch {
-                mutableApps.value = getAppItems()
-            }
+            updateMultiplePackages(packageName, user)
         }
 
         override fun onPackagesUnavailable(
             packageName: Array<out String>?, user: UserHandle?, replacing: Boolean
         ) {
+            updateMultiplePackages(packageName, user)
+        }
+
+        private fun updateSinglePackage(packageName: String?, user: UserHandle?) {
             viewModelScope.launch {
-                mutableApps.value = getAppItems()
+                withContext(Dispatchers.IO) {
+                    updateActivityList(user, packageName)
+                    postActivityList()
+                }
             }
+        }
+
+        private fun updateMultiplePackages(packageName: Array<out String>?, user: UserHandle?) {
+            viewModelScope.launch {
+                withContext(Dispatchers.IO) {
+                    packageName?.forEach {
+                        updateActivityList(user, it)
+                    }
+                    postActivityList()
+                }
+            }
+        }
+    }
+
+    companion object {
+        private fun getBounds(view: View): Rect {
+            val pos = IntArray(2).apply { view.getLocationOnScreen(this) }
+            return Rect(pos[0], pos[1], view.width, view.height)
+        }
+
+        private fun getAnimation(view: View): ActivityOptions {
+            return ActivityOptions.makeClipRevealAnimation(view, 0, 0, view.width, view.height)
         }
     }
 }
