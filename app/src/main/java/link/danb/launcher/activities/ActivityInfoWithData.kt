16e4925932512de2d8da5596ac2437dbd0da08a4
Dan Brunwasser <dbrunwasser@outlook.com>
2023-11-02 22:42:33 -0700

    Cleaned up code.

--- app/src/main/java/link/danb/launcher/activities/ActivityInfoWithData.kt
+++
@@ -1,6 +0,0 @@
-package link.danb.launcher.activities
-
-import android.content.pm.LauncherActivityInfo
-import link.danb.launcher.database.ActivityData
-
-data class ActivityInfoWithData(val info: LauncherActivityInfo, val data: ActivityData)
