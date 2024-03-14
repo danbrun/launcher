cae81be40eb3b8e7d9c101c4e466fe45f8b467dc
Dan Brunwasser <me@danb.link>
2024-03-13 23:24:36 -0700

    Updated Compose BOM.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,7 +81,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
 
     // Compose dependencies
-    implementation(platform("androidx.compose:compose-bom:2024.02.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.02.02"))
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
