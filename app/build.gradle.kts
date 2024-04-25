aafe475e1dd309b482c415c1ed8c0d9846d35828
Dan Brunwasser <me@danb.link>
2024-04-25 12:43:59 -0700

    Bumped version to 1.7.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "link.danb.launcher"
         minSdk = 28
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
