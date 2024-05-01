086616b56295ca1f22ce5aa9eab37ad297433ccd
Dan Brunwasser <me@danb.link>
2024-04-30 21:17:22 -0700

    Bump version to 1.8.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "link.danb.launcher"
         minSdk = 28
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
