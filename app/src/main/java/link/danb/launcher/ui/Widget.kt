d2f0e6f11dd28d9b4bde91890eeeb1a4e74138ba
Dan Brunwasser <me@danb.link>
2024-05-06 00:05:03 -0700

    Added long press detection back to widgets.

--- app/src/main/java/link/danb/launcher/ui/Widget.kt
+++ app/src/main/java/link/danb/launcher/ui/Widget.kt
@@ -9,6 +9,7 @@ import android.widget.FrameLayout
 import android.widget.ListView
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
@@ -19,6 +20,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -27,11 +29,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.PointerEventTimeoutCancellationException
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastAny
+import androidx.compose.ui.util.fastForEach
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.view.children
 import dagger.hilt.android.AndroidEntryPoint
@@ -49,8 +57,10 @@ fun Widget(
   widgetData: WidgetData,
   sizeRange: IntRange,
   modifier: Modifier = Modifier,
+  onLongClick: () -> Unit,
   setScrollEnabled: (Boolean) -> Unit,
   isInEditMode: Boolean,
+  confirm: () -> Unit,
   moveUp: () -> Unit,
   moveDown: () -> Unit,
   remove: () -> Unit,
@@ -60,35 +70,54 @@ fun Widget(
   var widgetFrame: WidgetFrame? by remember { mutableStateOf(null) }
   var isScrollEnabled: Boolean by remember { mutableStateOf(true) }
   val draggableState = rememberDraggableState { height = (height + it.toInt()).coerceIn(sizeRange) }
+  val hapticFeedback = LocalHapticFeedback.current
 
   Column(horizontalAlignment = Alignment.CenterHorizontally) {
     AndroidView(
       factory = { WidgetFrame(it).apply { widgetFrame = this } },
       modifier =
-        modifier.fillMaxWidth().height(with(LocalDensity.current) { height.toDp() }).pointerInput(
-          widgetFrame,
-          isScrollEnabled,
-        ) {
-          awaitPointerEventScope {
-            isScrollEnabled =
-              if (isScrollEnabled) {
-                val touchPos = awaitFirstDown(false).position
-                val list =
-                  widgetFrame?.let {
-                    val framePos = it.boundsOnScreen
-                    it.getListViewContaining(
-                      touchPos.x.toInt() + framePos.left,
-                      touchPos.y.toInt() + framePos.top,
-                    )
-                  }
-                list == null
-              } else {
-                waitForUpOrCancellation()
-                true
+        modifier
+          .fillMaxWidth()
+          .height(with(LocalDensity.current) { height.toDp() })
+          .pointerInput(1) {
+            awaitEachGesture {
+              awaitFirstDown(true, PointerEventPass.Initial)
+              try {
+                withTimeout(viewConfiguration.longPressTimeoutMillis) {
+                  waitForUpOrCancellation(PointerEventPass.Initial)
+                }
+              } catch (_: PointerEventTimeoutCancellationException) {
+                hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+                onLongClick()
+
+                do {
+                  val event = awaitPointerEvent(PointerEventPass.Initial)
+                  event.changes.fastForEach { it.consume() }
+                } while (event.changes.fastAny { it.pressed })
               }
-            setScrollEnabled(isScrollEnabled)
+            }
           }
-        },
+          .pointerInput(widgetFrame, isScrollEnabled) {
+            awaitPointerEventScope {
+              isScrollEnabled =
+                if (isScrollEnabled) {
+                  val touchPos = awaitFirstDown(false).position
+                  val list =
+                    widgetFrame?.let {
+                      val framePos = it.boundsOnScreen
+                      it.getListViewContaining(
+                        touchPos.x.toInt() + framePos.left,
+                        touchPos.y.toInt() + framePos.top,
+                      )
+                    }
+                  list == null
+                } else {
+                  waitForUpOrCancellation()
+                  true
+                }
+              setScrollEnabled(isScrollEnabled)
+            }
+          },
       onReset = { it.setAppWidget(null) },
       onRelease = { it.setAppWidget(null) },
       update = {
@@ -99,6 +128,15 @@ fun Widget(
 
     AnimatedVisibility(visible = isInEditMode) {
       Row {
+        TabButtonGroup {
+          TabButton(
+            painterResource(R.drawable.baseline_check_24),
+            stringResource(R.string.done),
+            isChecked = false,
+            onClick = confirm,
+          )
+        }
+
         TabButtonGroup {
           TabButton(
             painterResource(R.drawable.baseline_arrow_downward_24),
@@ -116,6 +154,7 @@ fun Widget(
                   onDragStopped = { setHeight(height) },
                 )
                 .padding(4.dp),
+            tint = MaterialTheme.colorScheme.primary,
           )
           TabButton(
             painterResource(R.drawable.baseline_arrow_upward_24),
