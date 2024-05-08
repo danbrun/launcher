6efa476859e411fe2466275edecd6fcf1c32e69e
Dan Brunwasser <me@danb.link>
2024-05-07 23:54:10 -0700

    Refactored some activity code.

--- app/src/main/java/link/danb/launcher/LauncherActivity.kt
+++ app/src/main/java/link/danb/launcher/LauncherActivity.kt
@@ -6,14 +6,14 @@ import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.getSystemService
+import androidx.fragment.app.commitNow
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LauncherActivity : AppCompatActivity() {
-
-  private val roleManager: RoleManager by lazy { getSystemService<RoleManager>()!! }
 
   private val setHomeActivityResultLauncher =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
@@ -22,6 +22,8 @@ class LauncherActivity : AppCompatActivity() {
       null
     }
 
+  private var hasRequestedHomeRole: Boolean = false
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
@@ -29,10 +31,9 @@ class LauncherActivity : AppCompatActivity() {
     setContentView(R.layout.launcher_activity)
 
     if (savedInstanceState == null) {
-      supportFragmentManager
-        .beginTransaction()
-        .replace(R.id.activity_frame, LauncherFragment())
-        .commit()
+      supportFragmentManager.commitNow { replace(R.id.activity_frame, LauncherFragment()) }
+    } else {
+      hasRequestedHomeRole = savedInstanceState.getBoolean(EXTRA_HAS_REQUESTED_HOME_ROLE)
     }
 
     onBackPressedDispatcher.addCallback(
@@ -42,15 +43,37 @@ class LauncherActivity : AppCompatActivity() {
         }
       }
     )
+  }
 
+  override fun onResume() {
+    super.onResume()
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && !hasRequestedHomeRole) {
+      hasRequestedHomeRole = true
+      requestHomeRole()
+    }
+  }
+
+  override fun onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+
+    outState.putBoolean(EXTRA_HAS_REQUESTED_HOME_ROLE, hasRequestedHomeRole)
+  }
+
+  @RequiresApi(Build.VERSION_CODES.Q)
+  private fun requestHomeRole() {
+    val roleManager: RoleManager = getSystemService() ?: return
     if (
-      Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q &&
-        roleManager.isRoleAvailable(RoleManager.ROLE_HOME) &&
+      roleManager.isRoleAvailable(RoleManager.ROLE_HOME) &&
         !roleManager.isRoleHeld(RoleManager.ROLE_HOME)
     ) {
       setHomeActivityResultLauncher?.launch(
         roleManager.createRequestRoleIntent(RoleManager.ROLE_HOME)
       )
     }
+  }
+
+  companion object {
+    private const val EXTRA_HAS_REQUESTED_HOME_ROLE = "extra_has_requested_home_role"
   }
 }
