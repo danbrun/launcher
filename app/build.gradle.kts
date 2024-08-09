569d303983c27b042a93234fce57e2c25b7e0c1a
Dan Brunwasser <me@danb.link>
2024-08-09 09:01:33 -0700

    Bump version to 1.13.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "link.danb.launcher"
         minSdk = 28
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.12"
+        versionCode = 14
+        versionName = "1.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
