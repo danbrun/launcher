4e9c83a675558dcd526b5fcb0beebbd608bb2cb0
Dan Brunwasser <me@danb.link>
2024-10-20 16:36:04 -0700

    Use BundleCompat.

--- app/src/main/java/link/danb/launcher/extensions/BundleExtensions.kt
+++ app/src/main/java/link/danb/launcher/extensions/BundleExtensions.kt
@@ -1,17 +1,9 @@
 package link.danb.launcher.extensions
 
-import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
+import androidx.core.os.BundleCompat
 
-/**
- * Convenience method to get a [Parcelable] from a [Bundle] using the appropriate API.
- *
- * The underlying method was deprecated in SDK 33 but we will continue using it until SDK 34 due to
- * [implementation bugs in SDK 33](https://issuetracker.google.com/issues/240585930#comment6).
- */
+/** Convenience method to get a [Parcelable] from a [Bundle] using the appropriate API. */
 inline fun <reified T> Bundle.getParcelableCompat(key: String): T? =
-  when {
-    Build.VERSION.SDK_INT > Build.VERSION_CODES.TIRAMISU -> getParcelable(key, T::class.java)
-    else -> @Suppress("DEPRECATION") getParcelable(key) as T?
-  }
+  BundleCompat.getParcelable(this, key, T::class.java)
