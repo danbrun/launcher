69e3a14a97bf8fba8201dcfffd88b1360326c915
Dan Brunwasser <me@danb.link>
2024-11-09 17:58:11 -0800

    Update dependencies and min sdk.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "link.danb.launcher"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "link.danb.launcher"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 24
         versionName = "1.24"
 
@@ -65,15 +65,15 @@ android {
 dependencies {
 
     // AndroidX dependencies
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.collection:collection-ktx:1.4.4")
+    implementation("androidx.collection:collection-ktx:1.4.5")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
-    implementation("androidx.fragment:fragment-ktx:1.8.4")
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
     implementation("androidx.palette:palette-ktx:1.0.0")
     implementation("androidx.sqlite:sqlite-ktx:2.4.0")
-    implementation("androidx.work:work-runtime-ktx:2.9.1")
-    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.work:work-runtime-ktx:2.10.0")
+    implementation("androidx.activity:activity-compose:1.9.3")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
@@ -88,7 +88,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
 
     // Compose dependencies
-    implementation(platform("androidx.compose:compose-bom:2024.09.03"))
+    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -105,7 +105,7 @@ dependencies {
     ksp("com.google.dagger:hilt-compiler:$daggerVersion")
 
     // Lifecycle dependencies
-    val lifecycleVersion = "2.8.6"
+    val lifecycleVersion = "2.8.7"
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-reactivestreams-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
@@ -114,7 +114,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
 
     // Navigation dependencies
-    val navigationVersion = "2.8.2"
+    val navigationVersion = "2.8.3"
     implementation("androidx.navigation:navigation-compose:$navigationVersion")
     implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
     implementation("androidx.navigation:navigation-runtime-ktx:$navigationVersion")
